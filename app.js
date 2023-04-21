d010d385351b041f4017d090e3f5d4e66c933ed9
JuliusMatadamas <juliusmatadamas@outlook.com>
2023-04-20 20:35:17 -0500

    Se prueba el archivo gitignore

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ app.use(cors());
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Escuchando en el puerto: ${port}`);
+    console.log(`Ahora el puerto es el: ${port}`);
 });
