a34a3e469d28a53858fef8ecb7fe91441f8d3265
JuliusMatadamas <juliusmatadamas@outlook.com>
2023-04-24 19:58:58 -0500

    Se crea la validacion de la ruta tracks.

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const {createItem, readItems, readItem, updateItem, deleteItem} = require("../controllers/tracks");
 const router = express.Router();
+const {validatorCreateItem} = require("../validators/tracks");
 
 router.get('/', readItems);
 router.get('/:id', readItem);
-router.post('/', createItem);
+router.post('/', validatorCreateItem, createItem);
 
 module.exports = router;
