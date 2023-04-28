9adba0616b671c2e2a992ae73e6495b66246f375
JuliusMatadamas <juliusmatadamas@outlook.com>
2023-04-28 16:08:07 -0500

    Se agrega el middleware para la autenticacion del usuario.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,40 @@
 const {matchedData} = require("express-validator");
-const {encrypt} = require("../utils/handlePassword");
+const {encrypt, compare} = require("../utils/handlePassword");
 const {usersModel} = require("../models");
 const {tokenSign} = require("../utils/handleJwt");
 const {handleHttpErrors} = require("../utils/handleErrors");
 
+
+const loginController = async (req, res) => {
+    try {
+        req = matchedData(req);
+        const user = await usersModel.findOne({email:req.email}).select('email name password role');
+
+        if (!user) {
+            handleHttpErrors(res, `Estas credenciales no coinciden con nuestros registros: ${err.message}`, 404);
+            return;
+        }
+
+        const passwordHash = user.password;
+        const check = await compare(req.password, passwordHash);
+
+        if (!check) {
+            handleHttpErrors(res, `Estas credenciales no coinciden con nuestros registros: ${err.message}`, 401);
+            return;
+        }
+
+        user.set('password', undefined, {strict:false});
+        const data = {
+            token: await tokenSign(user),
+            user
+        }
+
+        res.send({data})
+    } catch (err) {
+        console.log(err);
+        handleHttpErrors(res, `Error al intentar validar las credenciales`);
+    }
+}
 
 const registerController = async (req, res) => {
     try {
@@ -18,8 +49,8 @@ const registerController = async (req, res) => {
         }
         res.send({data});
     } catch (err) {
-        handleHttpErrors(res, `Error al intentar registrar el usuario: ${err.message}`);
+        handleHttpErrors(res, `Error al intentar registrar el usuario`);
     }
 }
 
-module.exports = { registerController }
+module.exports = { registerController, loginController }
--- routes/auth.js
+++ routes/auth.js
@@ -4,11 +4,10 @@ const {encrypt, compare} = require("../utils/handlePassword");
 const {usersModel} = require("../models");
 const {matchedData} = require("express-validator");
 const {tokenSign} = require("../utils/handleJwt");
-const {registerController} = require("../controllers/auth");
+const {registerController, loginController} = require("../controllers/auth");
 const router = express.Router();
 
-router.post("/login", validatorLogin, (req, res) => {
-});
+router.post("/login", validatorLogin, loginController);
 
 router.post("/register", validatorRegister, registerController);
 
